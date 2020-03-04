c3402d5591c1fa61f688ef80c6571551a7cb6b13
Sven <60364068+at-Sven@users.noreply.github.com>
2020-03-04 15:55:57 +0100

    Erste Suche funzt

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -16,13 +16,27 @@ interface State {
 class Movies extends React.Component<any, State> {
 
     componentDidMount() {
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=a69372d529161c3bf5f724875b197064&language=de&query=Highlander&page=1&include_adult=false')
+        axios.get('https://api.themoviedb.org/3/movie/popular?api_key=a69372d529161c3bf5f724875b197064&language=en-US&page=1')
             .then((res) => {
-                this.setState({ movies: res.data });
+                this.setState({ movies: res.data, value: '' });
             })
     }
 
-    handleChange = (e: any) => {
+    search = async (query: any) => {
+        if (query.length >= 3) {
+            let res = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=a69372d529161c3bf5f724875b197064&language=de&query=${query}&page=1&include_adult=false`);
+            let data = res.data;
+            // debugger
+            this.setState({ movies: data });
+        } else {
+            //TODO
+        }
+
+    };
+
+
+    onChangeHandler = async (e: any) => {
+        this.search(e.target.value);
         this.setState({ value: e.target.value });
     };
 
@@ -31,25 +45,28 @@ class Movies extends React.Component<any, State> {
     }
 
     render() {
-        if (!this.state || !this.state.movies) { return null }
+        if (!this.state) { return null }
         return (
             <div className="content" >
                 <div className="searchField">
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    <input type="text" value={this.state.value} onChange={this.onChangeHandler} />
                 </div>
-                <div className="searchResult">
-                    {this.state.movies.results.map((movie: any) => (
-                        <div key={movie.id} className="moviePosterFrame"
-                            onClick={() => this.props.history.push(`/movies/${movie.id}`)}>
-                            <img
-                                src={`http://image.tmdb.org/t/p/w185/${movie.poster_path}`}
-                                onError={this.imgError}
-                                alt="Movieposter"
-                            />
-                            <p>{movie.title}</p>
-                        </div>
-                    ))}
-                </div>
+                {this.state.movies && (
+                    <div className="searchResult">
+
+                        {this.state.movies.results.map((movie: any) => (
+                            <div key={movie.id} className="moviePosterFrame"
+                                onClick={() => this.props.history.push(`/movies/${movie.id}`)}>
+                                <img
+                                    src={`http://image.tmdb.org/t/p/w185/${movie.poster_path}`}
+                                    onError={this.imgError}
+                                    alt="Movieposter"
+                                />
+                                <p>{movie.title}</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
 
         );
